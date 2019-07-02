acb54539f7ebbeb2e695733ab547a1f07f8e389e
Ayush Rastogi <31179349+ayush1359@users.noreply.github.com>
2019-07-02 10:50:18 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-for (var i = 0; i < names.length; i++) {
-  var firstLetter = names[i].charAt(0).toLowerCase();
-  if (firstLetter === 'j') {
-    speak1(names[i]);
-  } else {
-    speak(names[i]);
-  }
-}
